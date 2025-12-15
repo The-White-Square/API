93e431a467a26ff85b54d8d2f051175c3b825462
Jaroslav <165400405+DrJaroslav@users.noreply.github.com>
2025-12-16 01:51:27 +0200

    Stage and commit not staged files

--- GameApp/GameApp.Service/Options/GalleryOptions.cs
+++ GameApp/GameApp.Service/Options/GalleryOptions.cs
@@ -3,4 +3,5 @@ namespace GameApp.Service.Options;
 public sealed class GalleryOptions
 {
     public string ImagesRoot { get; set; } = "wwwroot/images";
+    public string DrawingsRoot { get; set; } = "wwwroot/drawings";
 }
